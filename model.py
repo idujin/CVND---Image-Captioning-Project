ed0c6cf6191bba0108ee41ec454cd012e88fd1a7
idujin <yujin.gha@gmail.com>
2022-04-27 20:16:53 +0900

    initial model and hyper params were added.

--- model.py
+++ model.py
@@ -23,10 +23,23 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, 
+                            dropout=0.0, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        # remove <end>
+        captions = self.embed(captions[:,:-1])
+        # features.shape: [batch_size, embed_size]
+        # captions.shape: [batch_size, caption_len, embed_size]
+        input_x = torch.cat((features.unsqueeze(1), captions), dim = 1)
+        
+        output, _ = self.lstm(input_x)
+        output = self.fc(output)
+        
+        return output
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
